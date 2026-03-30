41bbb3411af85155eb1bef1b553722991d185fbb
dodo5517 <kh9015548@gmail.com>
2026-03-30 17:26:25 +0900

    add: 프로젝트 기초 파일 구성 및 의존성 설정

---
+++ src/main/java/com/rutina/rutinabackend/RutinaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rutina.rutinabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutinaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RutinaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rutina/rutinabackend/RutinaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rutina.rutinabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutinaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
