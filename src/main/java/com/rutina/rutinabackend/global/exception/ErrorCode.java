6b49682760cd0211830c92257339827542bfec40
dodo5517 <kh9015548@gmail.com>
2026-04-09 23:44:17 +0900

    feat: 토큰 재발급 에러코드 추가

--- src/main/java/com/rutina/rutinabackend/global/exception/ErrorCode.java
+++ src/main/java/com/rutina/rutinabackend/global/exception/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "AUTH_001", "이미 사용 중인 이메일입니다."),
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "AUTH_002", "이메일 또는 비밀번호가 올바르지 않습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "AUTH_003", "존재하지 않는 사용자입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_004", "유효하지 않거나 만료된 리프레시 토큰입니다."),
 
     // 공통
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "COMMON_001", "입력값이 올바르지 않습니다.");
