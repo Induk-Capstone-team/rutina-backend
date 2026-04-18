e5438bf6cfc17b7454bd7861f047aa5aee2da933
dodo5517 <kh9015548@gmail.com>
2026-04-19 01:13:55 +0900

    refactor: 미사용 TokenRefreshRequest DTO 제거

--- src/main/java/com/rutina/rutinabackend/domain/refreshToken/dto/TokenRefreshRequest.java
+++
@@ -1,8 +0,0 @@
-package com.rutina.rutinabackend.domain.refreshToken.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record TokenRefreshRequest(
-        @NotBlank String refreshToken
-) {
-}
