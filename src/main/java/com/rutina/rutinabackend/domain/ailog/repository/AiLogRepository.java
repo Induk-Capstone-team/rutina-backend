650493c115dda57582c623537f78a489571ed66f
Jihyeon Song <songsong7878@naver.com>
2026-04-27 00:05:50 +0900

    fix: Ai 로그 조회 메서드 수정

--- src/main/java/com/rutina/rutinabackend/domain/ailog/repository/AiLogRepository.java
+++ src/main/java/com/rutina/rutinabackend/domain/ailog/repository/AiLogRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AiLogRepository extends JpaRepository<AiLog, Long> {
-    List<AiLog> findByUserId(Long userId);
-    List<AiLog> findByRoutineId(Long routineId);
+    //기본적인 유저 조회
+    List<AiLog> findByUser_Id(Long userId);
+    //추가하기 버튼 눌렀을 때, 사용자가 선택한 optionId가 어떤 루틴 제목인지 알기 위함
+    Optional<AiLog> findByIdAndUser_Id(Long id, Long userId);
 }
