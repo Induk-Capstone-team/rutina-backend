be441d7c4f262d25e6e3359cd99237dda5adc1b1
swon0913 <wonjunn912@naver.com>
2026-04-29 20:52:03 +0900

    fix: OAuth2 로그인 refresh token 저장 트랜잭션 추가

--- src/main/java/com/rutina/rutinabackend/domain/refreshToken/repository/RefreshTokenRepository.java
+++ src/main/java/com/rutina/rutinabackend/domain/refreshToken/repository/RefreshTokenRepository.java
@@ -3,6 +3,7 @@ package com.rutina.rutinabackend.domain.refreshToken.repository;
 import com.rutina.rutinabackend.domain.refreshToken.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -11,5 +12,6 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     Optional<RefreshToken> findByTokenValue(String tokenValue);
 
+    @Transactional
     void deleteByUserIdAndDevice(Long userId, String device);
 }
