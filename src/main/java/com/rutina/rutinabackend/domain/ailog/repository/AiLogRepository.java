fcb5a5234ebce90e7583a335505ae3567ab7d4f4
Jihyeon Song <songsong7878@naver.com>
2026-05-10 20:30:20 +0900

    fix: 누락 import 추가

--- src/main/java/com/rutina/rutinabackend/domain/ailog/repository/AiLogRepository.java
+++ src/main/java/com/rutina/rutinabackend/domain/ailog/repository/AiLogRepository.java
@@ -1,8 +1,11 @@
 package com.rutina.rutinabackend.domain.ailog.repository;
 
 import com.rutina.rutinabackend.domain.ailog.entity.AiLog;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.OffsetDateTime;
 
 import java.util.List;
 import java.util.Optional;
